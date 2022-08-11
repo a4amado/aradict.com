2dfabf2e1b9eb815c9cdc70f213b7d0f3eb9f61d
Ahmad Addel <84184203+a4addel@users.noreply.github.com>
2022-08-11 13:23:00 +0200

    fix i18n path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
+const path  = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "ar",
     locales: ["en", "ar", "es", "nl", "id"],
-    localePath: "./public/assets",
+    localePath: path.resolve("./json")
   },
 };
--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
+const { i18n } = require('./next-i18next.config');
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { domains: ["abadis.ir", "www.svgrepo.com", "img.icons8.com"] },
-  i18n: {
-    locales: ["en", "ar", "es", "nl", "id"],
-    defaultLocale: "ar",
-    localePath: "./public/assets",
-    localeDetection: false,
-  },
+  i18n,
 };
 
 module.exports = nextConfig;
